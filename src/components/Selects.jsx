3ddd54e4fc7e7074e08840bcd7a7c5adeb9154a3
Satabdi Rani <32415291+Satabdi-Rani@users.noreply.github.com>
2023-07-31 13:07:52 +0600

    Image Section Added

---
+++ src/components/Selects.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import BoraBora from '../assets/borabora.jpg';
+import BoraBora2 from '../assets/borabora2.jpg';
+import Maldives from '../assets/maldives.jpg';
+import Maldives2 from '../assets/maldives2.jpg';
+import Maldives3 from '../assets/maldives3.jpg';
+import keyWest from '../assets/keywest.jpg';
+import SelectsCard from './SelectsCard';
+
+const Selects = () => {
+  return (
+    <div className='max-w-[1240px] mx-auto px-4 py-16 grid sm:grid-cols-2 lg:grid-cols-3 gap-4'>
+    <SelectsCard bg={BoraBora} text='Bora Bora' />
+    <SelectsCard bg={BoraBora2} text='Maldives' />
+    <SelectsCard bg={Maldives} text='Jamaica' />
+    <SelectsCard bg={Maldives2} text='Antartica' />
+    <SelectsCard bg={Maldives3} text='Switzerland' />
+    <SelectsCard bg={keyWest} text='Key west' />
+
+    </div>
+  )
+}
+
+export default Selects
